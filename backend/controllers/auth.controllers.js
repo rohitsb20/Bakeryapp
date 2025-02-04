9dece27f4c17a5cf471e7f003cc5718c0b1cf981
rohit <rohitsb96134@gmail.com>
2025-02-04 17:42:01 +0530

    refactor signup controller for improved validation and error handling

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,58 +1,51 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/token.js";
+import User from "../modals/user.modal.js"
 
 export const signup = async (req, res) => {
+  try {
+    const { fullname, email, password, confirmPassword } = req.body;
+    if (!fullname || !email || !password || !confirmPassword) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ msg: "Password must be atleast 6 characters long" });
+    }
 
-    try {
-        const { fullname, email, password, confirmPassword } = req.body;
-                if(!fullname || !email || !password || !confirmPassword){
-                    return res.status(400).json({msg: "All fields are required"});
-                }
-                if(password.length < 6){
-                    return res.status(400).json({msg: "Password must be atleast 6 characters long"});
-                }
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ msg: "Password and Confirm Password do not match" });
+    }
 
-                if(password !== confirmPassword){
-                    return res.status(400).json({msg: "Password and Confirm Password do not match"});
-                }
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email already exists" });
+    }
 
-                const userExist = await UserActivation.findOne({email});
-                if (userExist) {
-                  return res
-                    .status(400)
-                    .json({ msg: "User with this email already exists" });
-                }
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-               const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({
+      fullname,
+      email,
+      password: hashedPassword,
+    });
 
-                const newUser = new User({
-                    fullname,
-                    email,
-                    password: hashedPassword
-                });
-
-                if (newUser) {
-      
+    if (newUser) {
+      generateToken(newUser._id, res);
       await newUser.save();
       res.status(201).json({
         _id: newUser._id,
-        fullName: newUser.fullName,
+        fullname: newUser.fullname,
         email: newUser.email,
-       
       });
-   
-    } } catch (error) {
-        console.log("Error in signup controller", error);
-     res.status(500).json({ error: "Internal server error" });
-        
     }
-
-
-
-
-
-
-
-
-
-
-}
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
