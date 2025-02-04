7230f985544c252c828ad1791ff38f7b029f4b58
rohit <rohitsb96134@gmail.com>
2025-02-04 15:25:08 +0530

    implement routing and load FlyonUI on location change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
+import  { useEffect } from "react";
+import { useLocation, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const loadFlyonui = async () => {
+      await import("flyonui/flyonui");
+      window.HSStaticMethods.autoInit();
+    };
+    loadFlyonui();
+  }, [location.pathname]);
+
   return (
-    <>
-      <div>hello</div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
