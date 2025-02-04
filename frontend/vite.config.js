2ba863b4b3ab73e209002d052250f912fbace14e
rohit <rohitsb96134@gmail.com>
2025-02-04 17:42:45 +0530

    configure Vite server to proxy API requests to localhost:4000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+ 
   server: {
     host: "0.0.0.0",
-    
+    proxy: {
+      "/api": "http://localhost:4000",
+    },
   },
 });
