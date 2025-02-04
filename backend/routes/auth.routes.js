e8d0dc5cbd5c4d95dcb4ec07060120c626e72d85
rohit <rohitsb96134@gmail.com>
2025-02-04 18:15:50 +0530

    add login route to authentication API

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,9 @@
-import { signup } from "../controllers/auth.controllers.js";
+import { signup, login } from "../controllers/auth.controllers.js";
 import express from "express";
 
 const router = express.Router();
 
-router.post('/signup', signup);
+router.post("/signup", signup);
+router.post("/login", login);
 
 export default router;
