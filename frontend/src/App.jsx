1ee7ea156efbe08d266efcd7a3619118865f23f1
rohit <rohitsb96134@gmail.com>
2025-02-04 18:16:01 +0530

    add authentication redirection and toast notifications to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import  { useEffect } from "react";
-import { useLocation, Routes, Route } from "react-router-dom";
+import { useLocation, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import useAuthContext from "./zustand/useAuthContext";
+
 
 
 function App() {
   const location = useLocation();
+  const {authUser} = useAuthContext();
 
   useEffect(() => {
     const loadFlyonui = async () => {
@@ -18,9 +21,15 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      <Route path="/" element={authUser ? <Home/> : <Navigate to="/login" /> } />
+      <Route
+        path="/login"
+        element={authUser ? <Navigate to="/" /> : <Login />}
+      />
+      <Route
+        path="/signup"
+        element={authUser ? <Navigate to="/" /> : <Signup />}
+      />
     </Routes>
   );
 }
