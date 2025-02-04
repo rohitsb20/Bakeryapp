1ff80c1f9daa0c226d66c1cedacb2ebf8085da7a
rohit <rohitsb96134@gmail.com>
2025-02-04 16:21:19 +0530

    add signup controller for user registration

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,24 @@
+
+
+export const signup = (req, res) => {
+
+    try {
+        const { fullname, email, password, confirmPassword } = req.body;
+   
+   
+    } catch (error) {
+        console.log("Error in signup controller");
+        throw new Error(error);
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
