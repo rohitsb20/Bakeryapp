c6c9d21359e413ecb88d7fcadd770798af37338e
rohit <rohitsb96134@gmail.com>
2025-02-04 15:25:00 +0530

    add FlyonUI integration and update Tailwind CSS configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flyonui/dist/js/*.js",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require("flyonui"), require("flyonui/plugin")],
+  flyonui: {
+    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "soft"]
+    darkTheme: "false", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include FlyonUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    vendors: false, // default is false when true add customize css for apexChart, editor.js, flatpickr, fullcalendar, notyf, raty-js
+    logs: true, // Shows info about FlyonUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
 
